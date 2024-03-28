06fdc57e143ec0af284788f90d43acd99dfc3246
rutracrafter <artur.melo.rodrigues@gmail.com>
2024-03-28 14:07:36 -0700

    Finished visual implementation of search and filters. Still need to add functionality with storing selected values and calling the Spoonacular API.

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -1,10 +1,26 @@
 function toggleMenuHidden() {
   document.getElementById("menu").classList.toggle("hidden");
-  const menuToggleButton = document.getElementById("menuToggleButton")
+  const menuToggleButton = document.getElementById("menuToggleButton");
   if (menuToggleButton.getAttribute('src') == 'assets/MenuIcon.svg') {
-    menuToggleButton.setAttribute('src', 'assets/CloseIcon.svg')
+    menuToggleButton.setAttribute('src', 'assets/CloseIcon.svg');
   }
   else if(menuToggleButton.getAttribute('src') == 'assets/CloseIcon.svg') {
-    menuToggleButton.setAttribute('src', 'assets/MenuIcon.svg')
+    menuToggleButton.setAttribute('src', 'assets/MenuIcon.svg');
   }
+}
+
+function toggleFilterCuisine() {
+  document.getElementById("cuisine-dropdown").classList.toggle("hidden");
+}
+
+function toggleFilterDiet() {
+  document.getElementById("diet-dropdown").classList.toggle("hidden");
+}
+
+function toggleFilterType() {
+  document.getElementById("type-dropdown").classList.toggle("hidden");
+}
+
+function toggleFilterTime() {
+  document.getElementById("time-dropdown").classList.toggle("hidden");
 }
