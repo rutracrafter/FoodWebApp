ce6ee22c18a9ffc88e57ad1cbbcd9375b7b6678e
rutracrafter <artur.melo.rodrigues@gmail.com>
2024-03-30 05:54:51 -0700

    Finished implementing search and filtering functionality, still need to implement pagination and finish other nav options and then the app will be mostly done.

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -54,9 +54,56 @@ function searchEnter(event) {
   if (event.key === "Enter") {
     event.preventDefault();
 
-    const apiUrl = "https://fakewebsite.fake";
-    // implement logic to actually query api and display returned data, remember pagination
+    // let apiUrl = "https://api.spoonacular.com/recipes/complexSearch?apiKey=ac8f41702dc6488f85d3dda5ccc52208&instructionsRequired=true&addRecipeInformation=true&number=6&offset=0";
+    // if (searchField != "") {
+    //   let newApiUrl = apiUrl + "&query=" + searchField;
+    //   apiUrl = newApiUrl;
+    // }
+    // if (cuisineFilter != "") {
+    //   let newApiUrl = apiUrl + "&cuisine=" + cuisineFilter;
+    //   apiUrl = newApiUrl;
+    // }
+    // if (dietFilter != "") {
+    //   let newApiUrl = apiUrl + "&diet=" + dietFilter;
+    //   apiUrl = newApiUrl;
+    // }
+    // if (typeFilter != "") {
+    //   let newApiUrl = apiUrl + "&type=" + typeFilter;
+    //   apiUrl = newApiUrl;
+    // }
+    // if (timeFilter != "") {
+    //   let newApiUrl = apiUrl + "&time=" + timeFilter;
+    //   apiUrl = newApiUrl;
+    // }
+    let apiUrl = 'http://localhost:3000/scripts/result1.json';
 
-    console.log(`${searchField} ${cuisineFilter} ${dietFilter} ${typeFilter} ${timeFilter}`);
+    fetch(apiUrl)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Error when calling ' + apiUrl);
+        }
+        return response.json();
+      })
+      .then(data => {
+        console.log(data);
+        let displayResults = document.getElementById("displayResults");
+        displayResults.innerHTML = "";
+        data.results.forEach(recipe => {
+          displayResults.innerHTML += `
+          <div class="w-[300px] h-[400px] bg-medium-cream flex flex-wrap rounded-md p-2 drop-shadow-md">
+            <img src=${recipe["image"]} alt="image of ${recipe["title"]}" width="300" height="300" class="rounded-md border-4 border-dark-cream" />
+            <h1 class="w-full text-xl font-medium -mb-2 text-center">${recipe["title"]}</h1>
+            <h2 class="w-full text-lg text-center">${recipe["readyInMinutes"]} minutes</h2>
+            <a href=${recipe["sourceUrl"]} target="_blank" class="w-[300px] h-12 break-all bg-dark-cream hover:drop-shadow-md p-2 rounded-full text-center border-2 border-black">Ingredients and instructions here!</a>
+          </div>
+          `;
+        });
+      })
+      .catch(error => {
+        console.log('Error: ', error);
+      })
+
+    // console.log(apiUrl);
+    // console.log(`${searchField} ${cuisineFilter} ${dietFilter} ${typeFilter} ${timeFilter}`);
   }
 }
